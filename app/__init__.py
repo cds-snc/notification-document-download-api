33285696177d09d0b115ab5200741f528e7e2d35
Jimmy Royer <jimleroyer@gmail.com>
2021-12-06 18:30:40 -0500

    Removed GDSMetrics wrapper for promotheus (#49)
    * Removed GDSMetrics wrapper for promotheus
    
    * Fix improt
    
    * Removed gds-metrics dependency

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ import os
 
 from flask import Flask
 from notifications_utils import logging, request_helper
-from gds_metrics import GDSMetrics
 
 from app.config import configs
 from app.utils.store import DocumentStore
@@ -10,7 +9,6 @@ from app.utils.antivirus import AntivirusClient
 
 document_store = DocumentStore() # noqa, has to be imported before views
 antivirus_client = AntivirusClient() # noqa
-metrics = GDSMetrics() # noqa
 
 from .download.views import download_blueprint
 from .upload.views import upload_blueprint
@@ -26,7 +24,6 @@ def create_app():
 
     document_store.init_app(application)
     antivirus_client.init_app(application)
-    metrics.init_app(application)
 
     application.register_blueprint(download_blueprint)
     application.register_blueprint(upload_blueprint)
