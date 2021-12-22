13a63001dd0326234e3010c211b42680e5dd6266
Jimmy Royer <jimleroyer@gmail.com>
2021-12-22 11:21:03 -0500

    Removed default root and static routes (#50)
    * Removed default root and static routes
    
    * Updated the tests with removal of root support.

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.mark.parametrize(
     "endpoint",
-    ['/', '/_status'],
+    ['/_status'],
 )
 def test_healthcheck_endpoint(client, endpoint):
     response = client.get(endpoint)
