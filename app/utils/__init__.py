101d3a423aeef09cbeeef3162f6829aade94933b
Jimmy Royer <jimleroyer@gmail.com>
2022-01-19 01:18:52 -0500

    Increase buffer size to detect mime type.

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -3,7 +3,7 @@ import magic
 
 def get_mime_type(document_stream):
     try:
-        mime_type = magic.from_buffer(document_stream.read(1024), mime=True)
+        mime_type = magic.from_buffer(document_stream.read(2048), mime=True)
     finally:
         document_stream.seek(0)
 
