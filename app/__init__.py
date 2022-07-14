206999f608e4e5af0fca3a4ed592489947470bab
Stephen Astels <sastels@gmail.com>
2022-07-14 16:24:41 -0400

    add new endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from notifications_utils import logging, request_helper
+from notifications_utils.base64_uuid import base64_to_uuid, uuid_to_base64
+from werkzeug.routing import BaseConverter, ValidationError
 
 from app.config import configs
 from app.utils.store import DocumentStore
@@ -15,9 +17,25 @@ from .upload.views import upload_blueprint
 from .healthcheck import healthcheck_blueprint
 
 
+class Base64UUIDConverter(BaseConverter):
+    def to_python(self, value):
+        try:
+            return base64_to_uuid(value)
+        except ValueError:
+            raise ValidationError()
+
+    def to_url(self, value):
+        try:
+            return uuid_to_base64(value)
+        except Exception:
+            raise ValidationError()
+
+
 def create_app():
     application = Flask('app', static_folder=None)
     application.config.from_object(configs[os.environ['NOTIFY_ENVIRONMENT']])
+
+    application.url_map.converters['base64_uuid'] = Base64UUIDConverter
 
     request_helper.init_app(application)
     logging.init_app(application)
