c3ddc1bac4df2201a729a2932a88fa02eb092c15
Stephen Astels <sastels@gmail.com>
2022-08-16 14:49:26 -0400

    flake

--- app/download/views.py
+++ app/download/views.py
@@ -44,6 +44,7 @@ def download_document(service_id, document_id):
 
     return response
 
+
 @download_blueprint.route('/d/<base64_uuid:service_id>/<base64_uuid:document_id>', methods=['GET'])
 def download_document_b64(service_id, document_id):
     if 'key' not in request.args:
