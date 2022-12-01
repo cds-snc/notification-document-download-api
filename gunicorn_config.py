e5e0cb7ceb02bcfc41df9e16a9c268b131a59c2f
Steve Astels <sastels@users.noreply.github.com>
2022-12-01 15:17:05 -0500

    Python 3.10 (#88)

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,10 +3,10 @@ import sys
 import traceback
 
 workers = 4
-worker_class = "eventlet"
+worker_class = "gevent"
 worker_connections = 256
 bind = "0.0.0.0:{}".format(os.getenv("PORT"))
-accesslog = '-'
+accesslog = "-"
 
 # See AWS doc
 # > We also recommend that you configure the idle timeout of your application
@@ -25,7 +25,7 @@ def on_starting(server):
 def worker_abort(worker):
     worker.log.info("worker received ABORT {}".format(worker.pid))
     for threadId, stack in sys._current_frames().items():
-        worker.log.error(''.join(traceback.format_stack(stack)))
+        worker.log.error("".join(traceback.format_stack(stack)))
 
 
 def on_exit(server):
