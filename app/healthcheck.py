16ee4f52d25e55b52f5055ce6de55e651c8334ea
Michael Pond <michael.pond@cds-snc.ca>
2024-07-30 18:40:53 +0000

    just changing a name

--- app/healthcheck.py
+++ app/healthcheck.py
@@ -6,7 +6,7 @@ healthcheck_blueprint = Blueprint("healthcheck", __name__, url_prefix="")
 
 @healthcheck_blueprint.route("/_status")
 def status():
-    xray_recorder.begin_segment('AppStart')
+    xray_recorder.begin_segment('HealthCheck')
     xray_recorder.put_annotation('message', 'Xray: Someone hit the healthcheck endpoint')
     xray_recorder.end_segment()
     return "ok", 200
