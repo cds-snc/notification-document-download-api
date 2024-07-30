3f2597ac4cf4af3d056c8b63093c434a32ec2eba
Michael Pond <michael.pond@cds-snc.ca>
2024-07-30 19:13:53 +0000

    Adding a new route for xray testing

---
+++ app/xray_test.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, current_app
+from aws_xray_sdk.core import xray_recorder
+
+xray_blueprint = Blueprint("xray", __name__, url_prefix="")
+
+
+@xray_blueprint.route("/_xray")
+def status():
+    # Retrieve the current X-Ray segment
+    segment = xray_recorder.current_segment()
+    # Get the trace ID from the current segment
+    trace_id = segment.trace_id if segment else 'No segment'
+    # Log the trace ID
+    current_app.logger.info(f"Responding to request with X-Ray trace ID: {trace_id}")
+    current_app.logger.info("Xray Check Called")
+    return "Xray OK!", 200
--- application.py
+++ application.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+
 load_dotenv()
 
 # Set the XRAY_DAEMON_ADDRESS
@@ -20,8 +21,9 @@ application = create_app()
 # Apply the XRayMiddleware directly to the Flask application
 XRayMiddleware(application, xray_recorder)
 
-# Run this when the app starts
-xray_recorder.begin_segment('AppStart')
-application.logger.info('Logger: Application started')
-xray_recorder.put_annotation('message', 'Xray: Application started')
-xray_recorder.end_segment()
+# Retrieve the current X-Ray segment
+segment = xray_recorder.current_segment()
+# Get the trace ID from the current segment
+trace_id = segment.trace_id if segment else 'No segment'
+# Log the trace ID
+application.logger.info(f"Responding to request with X-Ray trace ID: {trace_id}")
