36a28e3001bc405e60b3dc900ddc8f2793d69b85
Michael Pond <michael.pond@cds-snc.ca>
2024-07-30 18:04:06 +0000

    bootstrapping xray middleware and example message

--- application.py
+++ application.py
@@ -1,8 +1,28 @@
 ##!/usr/bin/env python
 
+import os
 from app import create_app
 from dotenv import load_dotenv
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 load_dotenv()
 
+# Set the XRAY_DAEMON_ADDRESS
+os.environ['AWS_XRAY_DAEMON_ADDRESS'] = 'xray-daemon-aws-xray.xray.svc.cluster.local:2000'
+
+# Configure the xray_recorder
+xray_recorder.configure(service='notification-document-download-api')
+
+# Create the Flask application
 application = create_app()
+
+# Apply the XRayMiddleware directly to the Flask application
+XRayMiddleware(application, xray_recorder)
+
+# Run this when the app starts
+xray_recorder.begin_segment('AppStart')
+application.logger.info('Logger: Application started')
+xray_recorder.put_annotation('message', 'Xray: Application started')
+xray_recorder.end_segment()
+# Log a message when the app starts
