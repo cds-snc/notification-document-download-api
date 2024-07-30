2173651d5eda32f79200a72b650c051ff3f54f00
Mike Pond <32133001+P0NDER0SA@users.noreply.github.com>
2024-07-30 14:50:01 -0400

    Revert "Xray middleware (#176)" (#177)
    This reverts commit dbb0cb328d8412d321825943f3fb4f35643450fc.

--- app/healthcheck.py
+++ app/healthcheck.py
@@ -1,12 +1,9 @@
 from flask import Blueprint
-from aws_xray_sdk.core import xray_recorder
+
 
 healthcheck_blueprint = Blueprint("healthcheck", __name__, url_prefix="")
 
 
 @healthcheck_blueprint.route("/_status")
 def status():
-    xray_recorder.begin_segment('HealthCheck')
-    xray_recorder.put_annotation('message', 'Xray: Someone hit the healthcheck endpoint')
-    xray_recorder.end_segment()
     return "ok", 200
