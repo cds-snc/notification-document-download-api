8138d86282a9b6060f392c56a2b5c1615a305b08
Michael Pond <michael.pond@cds-snc.ca>
2024-07-30 18:25:13 +0000

    adding some healthcheck notes for xray

--- app/healthcheck.py
+++ app/healthcheck.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
-
+from aws_xray_sdk.core import xray_recorder
 
 healthcheck_blueprint = Blueprint("healthcheck", __name__, url_prefix="")
 
 
 @healthcheck_blueprint.route("/_status")
 def status():
+    xray_recorder.begin_segment('AppStart')
+    xray_recorder.put_annotation('message', 'Xray: Someone hit the healthcheck endpoint')
+    xray_recorder.end_segment()
     return "ok", 200
