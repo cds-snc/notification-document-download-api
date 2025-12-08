c2007173c509089730fe43819ac29b313677bbac
Pat Heard <patrick.heard@cds-snc.ca>
2025-12-08 13:03:47 -0500

    feat: support GuardDuty Malware Protection for S3 object tags (#225)
    Update the download and check verdict endpoints to use the GuardDuty Malware
    S3 scanning tag to determine if a file is malicious.

--- app/utils/scan_files.py
+++ app/utils/scan_files.py
@@ -4,6 +4,8 @@ from enum import Enum
 import requests
 from flask import current_app
 
+SCAN_FILES_SCAN_TAG = "av-status"
+
 
 # from https://github.com/cds-snc/scan-files/blob/630dd2f3c9dfbaae5438d5e2b3a656f0ef057abd/api/models/Scan.py#L16-L23
 class ScanVerdicts(Enum):
